fae133dea261c33e373c05617f8e53966081dae3
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2012-10-13 15:25:55 -0400

    added bootstrap to start ui layout merged audiostuff and parsestuff into controllers

--- controllers.js
+++ controllers.js
@@ -1,3 +1,65 @@
+// Setup Parse
+Parse.initialize("0vkAZmA6fG72LcEdrdhSFSGwak929wuNg7pYUnS3", "ajpyz4W4mSH1xqUqMuz8fZfuEwgtER9Aj7jSC8k1");
+
+function Authentication($scope) {
+    $scope.register = function() {
+        
+        var user = new Parse.User();
+        user.set("username", $scope.register.username);
+        user.set("password", $scope.register.password);
+        user.set("email",    $scope.register.email);
+        
+        user.signUp(null, {
+            success: function(user) {
+                $scope.$apply(function() {
+                    $scope.updateUser();
+                });
+            },
+            error: function(user,error) {
+                alert ("Error: " + error.code + " " + error.message);
+            }
+        });
+        
+    }
+    
+    $scope.login = function() {
+        
+        Parse.User.logIn($scope.login.username, $scope.login.password, {
+            success: function(user) {
+                $scope.$apply(function() {
+                    $scope.updateUser();
+                });
+            },
+            error: function(user, error) {
+                alert("Error: " + error.code + " " + error.message);
+            }
+        });
+        
+    }
+}
+
+function Global($scope) {
+    
+    $scope.currentUser = Parse.User.current();
+    
+    $scope.updateUser = function() {
+        $scope.currentUser = Parse.User.current();
+    }
+    
+    $scope.logout = function() {
+        Parse.User.logOut();
+        $scope.updateUser()
+    }
+    
+}
+
+function Library($scope) {
+    
+    
+    
+    
+}
+
 // Controller for the whole sequence editor      
 function SequenceEditor($scope) {
     
